c42ccc7756cf113ffd4e836d3762a6b342edad63
jhonnjc15 <jhonatanjcca@gmail.com>
2026-06-17 11:45:56 -0500

    Adaptando para multiples tablas y lambdas

--- main.tf
+++ main.tf
@@ -12,21 +12,27 @@ locals {
     if try(job_config.enabled, true)
   }
 
-  athena_sql_path = try(local.deploy_config.athena.sql_path, null)
-  athena_abs_path = local.athena_sql_path != null ? abspath("${path.module}/${local.athena_sql_path}") : null
+  enabled_athena_tables = {
+    for table_key, table_config in local.deploy_config.athena :
+    table_key => merge(
+      table_config,
+      {
+        sql_path = abspath("${path.module}/${table_config.sql_path}")
+      }
+    )
+    if try(table_config.enabled, true)
+  }
 
-  athena_config = merge(
-    try(local.deploy_config.athena, {}),
-    { sql_path = local.athena_abs_path }
-  )
-
-  lambda_source_path = try(local.deploy_config.lambda.source_path, null)
-  lambda_abs_path    = local.lambda_source_path != null ? abspath("${path.module}/${local.lambda_source_path}") : null
-
-  lambda_config = merge(
-    try(local.deploy_config.lambda, {}),
-    { source_path = local.lambda_abs_path }
-  )
+  enabled_lambdas = {
+    for lambda_key, lambda_config in local.deploy_config.lambda :
+    lambda_key => merge(
+      lambda_config,
+      {
+        source_path = abspath("${path.module}/${lambda_config.source_path}")
+      }
+    )
+    if try(lambda_config.enabled, true)
+  }
 
   common_tags = {
     environment = local.deploy_config.environment
@@ -49,17 +55,20 @@ module "glue_jobs" {
 }
 
 module "athena" {
-  source = "git::https://github.com/jhonnjc15/artifact3-terraform-templates.git//modules/athena?ref=main"
+  for_each = local.enabled_athena_tables
+  source   = "git::https://github.com/jhonnjc15/artifact3-terraform-templates.git//modules/athena?ref=main"
 
-  athena = local.athena_config
+  athena = each.value
   tags   = local.common_tags
 }
 
 module "lambda" {
-  source = "git::https://github.com/jhonnjc15/artifact3-terraform-templates.git//modules/lambda?ref=main"
+  for_each = local.enabled_lambdas
+  source   = "git::https://github.com/jhonnjc15/artifact3-terraform-templates.git//modules/lambda?ref=main"
 
   artifact_bucket = var.artifact_bucket
   lambda_role_arn = var.lambda_role_arn
-  lambda          = local.lambda_config
+  lambda          = each.value
+  code_prefix     = "lambda/code/${local.deploy_config.environment}"
   tags            = local.common_tags
 }
--- outputs.tf
+++ outputs.tf
@@ -6,22 +6,32 @@ output "script_s3_locations" {
   value = module.glue_jobs.script_s3_locations
 }
 
-output "athena_database_name" {
-  value = module.athena.database_name
+output "athena_database_names" {
+  value = {
+    for k, m in module.athena : k => m.database_name
+  }
 }
 
-output "athena_table_name" {
-  value = module.athena.table_name
+output "athena_table_names" {
+  value = {
+    for k, m in module.athena : k => m.table_name
+  }
 }
 
-output "athena_s3_location" {
-  value = module.athena.s3_location
+output "athena_s3_locations" {
+  value = {
+    for k, m in module.athena : k => m.s3_location
+  }
 }
 
-output "lambda_function_name" {
-  value = module.lambda.function_name
+output "lambda_function_names" {
+  value = {
+    for k, m in module.lambda : k => m.function_name
+  }
 }
 
-output "lambda_function_arn" {
-  value = module.lambda.function_arn
+output "lambda_function_arns" {
+  value = {
+    for k, m in module.lambda : k => m.function_arn
+  }
 }
