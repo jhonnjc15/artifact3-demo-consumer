b8f69992d4cc7234d533fe083e5545cbc3491363
jhonnjc15 <jhonatanjcca@gmail.com>
2026-06-17 10:30:49 -0500

    Actualizando athena module para dejar la responsabilidad de config en el archivo .sql

--- src/sql/create_table.sql
+++ src/sql/create_table.sql
@@ -10,7 +10,14 @@ PARTITIONED BY (ingestion_date STRING)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
 STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION 's3://bronze-1358-0891-8691/COMERCIAL/ybwds_copa_brz/'
 TBLPROPERTIES (
   'classification' = 'parquet',
+  'table_type' = 'EXTERNAL_TABLE',
+  'projection.enabled' = 'true',
+  'projection.ingestion_date.type' = 'date',
+  'projection.ingestion_date.range' = '2020-01-01,NOW',
+  'projection.ingestion_date.format' = 'yyyy-MM-dd',
+  'storage.location.template' = 's3://bronze-1358-0891-8691/COMERCIAL/ybwds_copa_brz/ingestion_date=${ingestion_date}/',
   'UpdatedByJob' = 'prueba-la-br'
 );
