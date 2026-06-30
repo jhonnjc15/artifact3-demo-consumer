814da3896d205a59225d2c4e880cb129b32c3449
jhonnjc15 <jhonatanjcca@gmail.com>
2026-06-30 15:49:31 -0500

    Prueba creación de tahlas modulo de athena

--- src/sql/ybwds_copa_brz/create_table_external_3.sql
+++ src/sql/ybwds_copa_brz/create_table_external_3.sql
@@ -1,22 +1,20 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS db_demo.ybwds_copa_brz_external_2 (
+CREATE EXTERNAL TABLE IF NOT EXISTS db_demo.ybwds_copa_brz_external_3(
   mandt STRING,
   kunnr STRING,
   matnr STRING,
   nt_wt_kg DOUBLE,
   cost_val_s DOUBLE,
-  vkorg STRING
+  vkorg STRING,
+  vtweg STRING,
+  prueba STRING,
+  fecha DATE,
+  ingestion_ts TIMESTAMP
 )
-PARTITIONED BY (
-  ingestion_date STRING
-)
-ROW FORMAT SERDE
-  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
-STORED AS INPUTFORMAT
-  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
-OUTPUTFORMAT
-  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
-LOCATION
-  's3://bronze-1358-0891-8691/COMERCIAL/ybwds_copa_brz/'
+PARTITIONED BY (ingestion_date STRING)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
+OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION 's3://bronze-1358-0891-8691/COMERCIAL/ybwds_copa_brz/'
 TBLPROPERTIES (
   'classification' = 'parquet',
   'table_type' = 'EXTERNAL_TABLE',
